6a13b4f10af010b3514f87b1a3e0be07a5e2029b
wangqiang <850994281@qq.com>
2019-06-30 17:42:39 +0800

    switch practice

---
+++ src/FisrtDemo.java
@@ -0,0 +1,13 @@
+public class FisrtDemo {
+    public static void main(String[] args) {
+        int num=10;
+        switch (num){
+            case 5:
+                System.out.println("5");
+                break;
+            case 10:
+                System.out.println("10");
+                break;
+        }
+    }
+}
